02134e6d2e81fc95b8490c50f8443d32f23eea8e
Inessa Babenko <iness.tt.eln@gmail.com>
2026-07-04 15:44:24 +0700

    add: receive messages via protobuf

--- telemetry/collector/src/main/java/ru/practicum/telemetry/collector/controller/CollectorController.java
+++ telemetry/collector/src/main/java/ru/practicum/telemetry/collector/controller/CollectorController.java
@@ -1,4 +1,35 @@
 package ru.practicum.telemetry.collector.controller;
 
-public class CollectorController {
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+import ru.yandex.practicum.grpc.telemetry.collector.CollectorControllerGrpc;
+import ru.yandex.practicum.grpc.telemetry.collector.CollectorResponse;
+import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
+import com.google.protobuf.util.JsonFormat;
+
+@Slf4j
+@GrpcService
+public class CollectorController extends CollectorControllerGrpc.CollectorControllerImplBase {
+
+    @Override
+    public void collectSensorEvent(SensorEventProto request, StreamObserver<CollectorResponse> responseObserver) {
+        try {
+            log.info("Echo received");
+            String json = JsonFormat.printer().preservingProtoFieldNames()
+                    .print(request);
+            log.info(json);
+            responseObserver.onNext(CollectorResponse.newBuilder().setMessage(json).build());
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            responseObserver.onError(new StatusRuntimeException(
+                    Status.INTERNAL
+                            .withDescription(e.getLocalizedMessage())
+                            .withCause(e)
+            ));
+        }
+    }
 }
+
